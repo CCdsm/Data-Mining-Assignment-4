f2ce33c4bd26746e2fdd491009dad388f88bfff1
CCdsm <53102982+CCdsm@users.noreply.github.com>
2025-05-09 07:39:04 +0800

    Update P1.py

--- P1.py
+++ P1.py
@@ -8,11 +8,9 @@ df = pd.read_csv('auto-mpg.data', sep=r'\s+', names=col_names, na_values='?')
 continuous_cols = ['mpg','displacement','horsepower','weight','acceleration']
 df['horsepower'] = df['horsepower'].fillna(df['horsepower'].mean())
 X = df[continuous_cols]
-
 clusterer = AgglomerativeClustering(n_clusters=3, linkage='average')
 labels = clusterer.fit_predict(X)
 df['cluster'] = labels
-
 cluster_stats = df.groupby('cluster')[continuous_cols].agg(['mean','var'])
 origin_stats  = df.groupby('origin')[continuous_cols].agg(['mean','var'])
 
@@ -20,6 +18,5 @@ print("=== Cluster statistics ===")
 print(cluster_stats)
 print("\n=== Origin statistics ===")
 print(origin_stats)
-
 print("\n=== Crosstab cluster vs origin ===")
 print(pd.crosstab(df['cluster'], df['origin']))
